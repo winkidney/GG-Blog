f4477d34616ee44b6e7e52af0ce53c26dfbd22e6
winkidney <winkidney@gmail.com>
2013-07-22 17:05:32 +0800

    将所有的静态文件集成到此处并添加安装说明

--- manage/views.py
+++ manage/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect,HttpResponse
 from django.contrib.auth import (authenticate, login ,logout)
 from django.template import RequestContext
-from blog.models import BasicSettings
+from blog.models import BasicSettings,Tags,ThreadTypes
 #own import 
 from pycms import settings
 
@@ -20,7 +20,10 @@ def home(request):
 @login_required(login_url=blog_login_url)
 def make_post(request):
     if request.method == 'GET':
-        #if request
-        return render_to_response('manage/make_post.html',{'static_root':static_root},context_instance=RequestContext(request))
+        #标签
+        tags = Tags.objects.all()
+        #分类
+        threadtypes = ThreadTypes.objects.all()
+        return render_to_response('manage/make_post.html',{'static_root':static_root,'tags':tags,'threadtypes':threadtypes},context_instance=RequestContext(request))
     if request.method == 'POST':
         return HttpResponse('all done')
