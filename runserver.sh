570f2d507fc9f733db45635c0bba7f972fec2152
root <root@kidney-PAN4>
2013-11-18 19:27:02 +0800

    ADD:restart script

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,2 @@
 #kill `cat blogfcgi.pid`
-python manage.py runfcgi method=threaded host=127.0.0.1 port=3033 maxchildren=300 pidfile=blogfcgi.pid
+python manage.py runfcgi method=threaded host=127.0.0.1 port=3033 maxchildren=300 pidfile=/tmp/blogfcgi.pid
